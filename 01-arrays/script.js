dabfeb59aab7a599ef03d262a775281578f87096
Michela <micheladellagatta1@gmail.com>
2025-10-08 14:37:55 +0200

    Fix console log message

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -11,7 +11,7 @@ const teachers = [
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = teachers[3];
-console.log('Nuovo quarto insegnante: '+fourthTeacher);
+console.log('Quarto insegnante: '+fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 teachers[4] = 'Patrick';
