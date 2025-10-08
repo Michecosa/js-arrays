52f0e60d922ec1d20e24e5451a2e7d6377f79d2b
Michela <micheladellagatta1@gmail.com>
2025-10-08 13:32:44 +0200

    Solve 01-arrays, exercise 4; Fix console.log outputs

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -11,18 +11,19 @@ const teachers = [
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = teachers[3];
-console.log('Quarto insegnante: '+fourthTeacher);
+console.log('Nuovo quarto insegnante: '+fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 teachers[4] = 'Patrick';
-console.log('Quinto insegnante: '+teachers[4]);
+console.log('Nuovo quinto insegnante: '+teachers[4]);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.pop();
-console.log('Insegnante rimosso: '+lastTeacher);
+console.log('Ultimo insegnante rimosso ('+lastTeacher+')');
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+console.log('Primo insegnante rimosso ('+firstTeacher+')');
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
